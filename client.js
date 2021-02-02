3a8cf6832d61bb93c871a447f33ffc757cdd32fe
jmbielejeski <jmbielejeski@gmail.com>
2021-02-02 10:15:02 -0700

    testing out some node

--- client.js
+++ client.js
@@ -1,2 +1,48 @@
-console.log('JS is running');
-console.log('Yay for node!!!');
+// No jQuery in Node
+// can only run one file at a time in node
+// running servers with node
+
+//Pull data from another file
+let cullenites = require('./cullenites.js');
+
+for (let student of cullenites) {
+  console.log(`Welcome to node week, ${student}`);
+}
+
+let age = 29;
+if (age > 23) {
+  console.log('you are an old man');
+}
+
+function birthday() {
+  age++;
+  return age;
+}
+
+console.log(`Happy birthday to you! You are, ${birthday()}`);
+console.log(`Happy birthday to you! You are, ${birthday()}`);
+console.log(`Happy birthday to you! You are, ${birthday()}`);
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+function headsOrTails() {
+  let heads = 1;
+  let tails = 2;
+
+  let flipResult = randomNumber(heads, tails);
+
+  if (flipResult === 1) {
+    return 'its heads!';
+  } else {
+    return 'its tails!';
+  }
+}
+
+console.log(headsOrTails());
+console.log(headsOrTails());
+console.log(headsOrTails());
+console.log(headsOrTails());
+console.log(headsOrTails());
+console.log(headsOrTails());
---
+++ cullenites.js
@@ -0,0 +1,11 @@
+let students = ['John', 'Michael', 'Will'];
+
+// export data from this file
+
+module.exports = students;
+
+// module.exports
+// means: "Here's the data I want to share"
+//
+// so... 'students' will be the return value
+// of require('./cullenites.js')
